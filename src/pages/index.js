ccd5c7ec71b043c21f6f30e77fb194aacc0d7aea
Hugh Guiney <hugh@hughguiney.com>
2019-08-18 23:31:18 -0400

    Alphabetize experience

--- src/pages/index.js
+++ src/pages/index.js
@@ -164,7 +164,7 @@ class Experience extends React.Component {
                 const timeOnJob = this.getDuration( node.frontmatter.startDate, node.frontmatter.endDate )
 
                 Experience.sections.forEach( ( section ) => {
-                  node.frontmatter[section]
+                  node.frontmatter[section] = node.frontmatter[section]
                     // Remove duplicates
                     .filter( ( item, index ) => node.frontmatter[section].indexOf( item ) === index )
                     .sort( this.alphabetize )
